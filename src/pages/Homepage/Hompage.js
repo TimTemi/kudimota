30961da623ac55dff745e42cf08c54792a77156c
unknown <spontaneousmanji@gmail.com>
2022-06-26 15:03:55 +0100

    feat: Homepage down for development

--- src/pages/Homepage/Hompage.js
+++ src/pages/Homepage/Hompage.js
@@ -1,9 +1,19 @@
+import Footer from "../../components/Footer/Footer";
+
 const Homepage = () => {
-  return(
+  return (
     <section>
-      <div>
-        
+      <div style={{ backgroundColor: "tomato" }}>
+        <h1>Homepage currently under development use</h1>
+        <br />
+        <h2>use the following urls to navigate app @</h2>
+        <br />
+        <h3>http://localhost:3000</h3>
+        <br />
+        <p>carSpecification: "/Userspec",</p>
+        <p>perdictedCar: "/Predictvalue",</p>
       </div>
+      <Footer />
     </section>
   );
 };
